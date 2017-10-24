01ef9053c5b2f7b52846382723b9e974a620e1d4
Scott Murray <rai.five@gmail.com>
2017-10-24 19:52:45 +0100

    All Tests Passing

--- app/src/test/java/example/codeclan/com/employeestartpoint/EmployeeTest.java
+++ app/src/test/java/example/codeclan/com/employeestartpoint/EmployeeTest.java
@@ -1,5 +1,7 @@
 package example.codeclan.com.employeestartpoint;
 
+import android.text.style.AbsoluteSizeSpan;
+
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -11,11 +13,18 @@ import static org.junit.Assert.*;
 public class EmployeeTest {
     Employee employee;
     Manager manager;
+    Engineer engineer;
+    Admin admin;
+    Director director;
 
     @Before
     public void before() {
-        employee = new Employee(1, "Wilma", "ABC12345", 50000.0);
-        manager = new Manager("Engineering", 2, "Wilma", "ABC12345", 50000.0);
+        employee = new Employee(1, "Gerry", "ABC12345", 50000.0);
+        engineer = new Engineer(1, "Scott", "ABC12345", 60000.0);
+        admin = new Admin(1, "Wilma", "ABC12345", 40000.0);
+        manager = new Manager("Engineering", 2, "Russell", "ABC12345", 80000.0);
+        director = new Director(1000000.0, "Marketing", 2, "Russell", "ABC12345", 80000.0);
+
     }
 //
 //    @Test
@@ -45,9 +54,26 @@ public class EmployeeTest {
 //    }
 
     @Test
-    public void canSetEmployeeName() {
-        employee.setName("Wilma");
-        assertEquals("Wilma", employee.getName());
+    public void canSetName() {
+        employee.setName("Ted");
+        engineer.setName("Bob");
+        admin.setName("Bill");
+        manager.setName("Toby");
+        assertEquals("Ted", employee.getName());
+        assertEquals("Bob", engineer.getName());
+        assertEquals("Bill", admin.getName());
+        assertEquals("Toby", manager.getName());
+    }
+
+    @Test
+    public void canGetDept(){
+        assertEquals("Engineering", manager.getDept());
+        assertEquals("Marketing", director.getDept());
+    }
+
+    @Test
+    public void canGetBudget(){
+        assertEquals(1000000.00, director.getBudget(), 0.01);
     }
 
 //    @Test
@@ -57,8 +83,21 @@ public class EmployeeTest {
 //    }
 
     @Test
-    public void canSetEmployeeSalary() {
+    public void canSetSalary() {
         employee.setSalary(30000.00);
+        director.setSalary(90000.00);
         assertEquals(30000.00, employee.getSalary(), 0.01);
+        assertEquals(90000.00, director.getSalary(), 0.01);
+
+    }
+
+    @Test
+    public void canRaiseSalary() {
+        employee.raiseSalary(1000.00);
+        director.raiseSalary(1000.00);
+        manager.raiseSalary(-5000.00);
+        assertEquals(51000.0, employee.getSalary(), 0.01);
+        assertEquals(81000.0, director.getSalary(), 0.01);
+        assertEquals(80000.00, manager.getSalary(), 0.01);
     }
 }
